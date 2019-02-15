f2175940ae32815924d41d01a09c22315b3dfddb
amegonza67 <ameliagonzalezlopez@gmail.com>
2019-02-15 13:43:31 +0100

    Tabla de multiplicar del 5

---
+++ PruebaGit/src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+
+	public static void main(String[] args) {
+		int numero = 5;
+		for (int i=1; i<=10; i++) {
+			System.out.println(numero + " x " + i + " = " + (numero*i));
+		}
+	}
+}
