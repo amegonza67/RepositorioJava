2ac7fd16199dff124bf02e477b8b5b1e86ff8f2b
amegonza67 <ameliagonzalezlopez@gmail.com>
2019-02-15 13:55:14 +0100

    Tabla de multiplicar de cualquier número introducido por teclado.

--- PruebaGit/src/Principal.java
+++ PruebaGit/src/Principal.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		int numero = 5;
+		Scanner lector = new Scanner(System.in);
+		System.out.println("¿La tabla de que número deseas? ");
+		int numero = lector.nextInt();
+		lector.nextLine(); 
+		lector.close();
 		for (int i=1; i<=10; i++) {
 			System.out.println(numero + " x " + i + " = " + (numero*i));
 		}
